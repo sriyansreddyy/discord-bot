ce06a59f0a263de4a2cb213880518f3d035fab1d
Alex Booker <booker@booker.codes>
2021-06-24 21:20:02 +0100

    handle steps that need reaction response

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bot = new Discord.Client({
   partials: ['MESSAGE', 'REACTION']
 })
 
-
 const { Pool, Client } = require('pg')
 require('dotenv').config()
 
@@ -49,12 +48,18 @@ const steps = [
     process: async (answer, member) => await member.setNickname(answer)
   },
   {
+    question: "What's your favourite programming language?",
+    process: console.log('user answered')
+  },
+  {
     question: `Fantastic. To access the sever, please click this
     link to connect your Scrimba account: https://scrimba.com/discord/connect`,
     process: (answer, member) => fetchScrimbaUser(member.id)
   },
   {
     question: 'Watch this then https://youtu.be/lPIi430q5fk respond with the ✅',
+    reaction: '✅',
+    process: () => console.log('process emojiii')
   }
 ]
 
@@ -84,7 +89,9 @@ bot.on('message', async message => {
       const nextStep = steps[index + 1]
 
       if (nextStep) {
-        await channel.send(nextStep.question)
+        const message = await channel.send(nextStep.question)
+        if(nextStep.reaction) 
+          message.react(nextStep.reaction)
       } else {
         await assignRegularMemberRole(member)
         await cleanup(channel)
@@ -95,11 +102,68 @@ bot.on('message', async message => {
   }
 })
 
-bot.on('messageReactionAdd', x => {
-  console.log('x', x)
+bot.on('messageReactionAdd', async (messageReaction, user) => {
+  const { partial, message, emoji } = messageReaction
+  if (partial) {
+    // TODO: process partial messages if needed
+    console.error('partial', partial)
+    // await messageReaction.fetch()
+  }
+
+  if (user.id === bot.user.id) {
+    console.log('it\'s just the botty bot')
+    return
+  }
+
+  const { channel}  = message
+
+  if (channel.type === "text" 
+    && channel.name.startsWith(WELCOME_PREFIX)) {
+    const onboardee = channel.name.split("-")[1]
+    const reactor = user.discriminator
+    if (reactor === onboardee) {
+      const messages = await channel.messages.fetch()
+      const botMessages = messages
+        .filter(message => message.author.id === bot.user.id)
+      const question = botMessages.first().content
+
+      console.log('question', question)
+      console.log('reaction', emoji.name)
+
+      const answer = emoji.name
+      const answers = { }
+      answers[question] = answer
+      const index = steps.findIndex(step => step.question === question)
+      const step = steps[index]
+      await step.process(answer, user)
+      const nextStep = steps[index + 1]
+      if (nextStep) {
+        await channel.send(nextStep.question)
+      } else {
+        const member = messageReaction.message.guild.members.cache.find(member => member.id === user.id)
+        await assignRegularMemberRole(member)
+        await cleanup(channel)
+        await sendWelcomeDirectMessage(user)
+      }
+    }
+  }
+
+
+
+
+  // console.log('messageReaction', messageReaction)
+
+
+  // console.log('partial?', partial)
+  // console.log('channel', channel)
+  // console.log('messageId', messageId)
+  // console.log('author', users)
+  // console.log('reactions', users)
+  // console.log('users', users)
+  // console.log('count', count)
 })
 
-const assignRegularMemberRole = (member) => member.roles.add(REGULAR_MEMBER_ROLE_ID)
+const assignRegularMemberRole = member => member.roles.add(REGULAR_MEMBER_ROLE_ID)
 const cleanup = channel => channel.delete()
 const sendWelcomeDirectMessage = member => member.send('hi')
 
