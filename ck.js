25343c95ccae07485fc43667934c3d4ab5a9ebea
Alex Booker <booker@booker.codes>
2021-08-27 10:57:28 +0100

    helper functions to interface with ConvertKit

--- ck.js
+++ ck.js
@@ -1,74 +1,51 @@
 const got = require('got') 
+require('dotenv').config()
 
-const email = 'booker@booker.codes'
-const CONVERT_KIT_API_SECRET = ''
+const { CONVERT_KIT_API_KEY, CONVERT_KIT_API_SECRET, CONVERT_KIT_TAG_ID, CONVERT_KIT_FORM_ID } = process.env
 
-function getSubscriberEndpoint() {
+const fetchSubscriber = async email => {
   const url = new URL('https://api.convertkit.com/v3/subscribers')
   url.searchParams.set('api_secret', CONVERT_KIT_API_SECRET)
   url.searchParams.set('email_address', email)
-  return url.toString()
-}
 
-async function getConvertKitSubscriber(email) {
-  const url = getSubscriberEndpoint(email)
-
-  const {
-    body: {
-      subscribers: [subscriber = {state: 'inactive'}] = []} = {},
-  } = await got(url.toString(), { 
+  const { body } = await got(url.toString(), {
     responseType: 'json'
   })
 
-  return subscriber.state === 'active' ? subscriber : null
+  return body.subscribers.shift()
 }
 
-
-const foo () => {
-  const discordTagId = ''
-  const subscriber = answers.email
-    ? await getConvertKitSubscriber(answers.email)
-    : null
+const addTag = async email => {
+  const subscriber = await fetchSubscriber(email)
+  console.log("subscriber", subscriber)
   if (subscriber) {
-    await got.post(
-      `https://api.convertkit.com/v3/tags/${discordTagId}/subscribe`,
+    const response = await got.post(
+      `https://api.convertkit.com/v3/tags/${CONVERT_KIT_TAG_ID}/subscribe`,
       {
         responseType: 'json',
         json: {
           api_key: CONVERT_KIT_API_KEY,
           api_secret: CONVERT_KIT_API_SECRET,
-          first_name: answers.name,
-          email: answers.email,
-          fields: {discord_user_id: member.id},
+          email: email
         },
       },
     )
+    console.log("addTag response", response.body)
   } else {
-    // the main deifference in subscribing to a tag and subscribing to a
-    // form is that in the form's case, the user will get a double opt-in
-    // email before they're a confirmed subscriber. So we only add the
-    // tag to existing subscribers who have already confirmed.
     await got.post(
-      `https://api.convertkit.com/v3/forms/${discordForm}/subscribe`,
+      `https://api.convertkit.com/v3/forms/${CONVERT_KIT_FORM_ID}/subscribe`,
       {
         responseType: 'json',
         json: {
           api_key: CONVERT_KIT_API_KEY,
           api_secret: CONVERT_KIT_API_SECRET,
-          first_name: answers.name,
-          email: answers.email,
-          fields: {discord_user_id: member.id},
-          // It would make sense to include the tag here, however, doing this
-          // will auto-confirm this new subscriber (no double-opt-in) which
-          // we do not want to do. Luckily, the discordForm adds this tag
-          // automatically so we don't need it anyway.
-          // tags: [discordTagId],
+          email: email
         },
       },
     )
-
   }
 }
 
-const x = getSubscriberEndpoint()
-console.log(x)
+(async () => {
+  await addTag("booker+4@booker.codes")
+})()
