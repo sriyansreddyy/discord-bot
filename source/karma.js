f12efcfdaee9ff188dd3571e818ba3a1aca5fad6
Alex Booker <booker@booker.codes>
2021-09-16 20:40:03 +0100

    add more information to the reputation message

--- source/karma.js
+++ source/karma.js
@@ -1,15 +1,20 @@
 const karma = bot => {
-  const NOTIFICATIONS_CHANNEL_ID = '888143560673747044'
+  const NOTIFICATIONS_CHANNEL_ID = '868130359106207844'
+  // const NOTIFICATIONS_CHANNEL_ID = '888143560673747044' // prod
   bot.on('messageReactionAdd', async (messageReaction, user) => {
     if (messageReaction.partial) {
       await messageReaction.fetch()
     }
     const { message, emoji } = messageReaction
+
+    console.log(message)
     const notificationsChannel = await bot
       .channels
       .fetch(NOTIFICATIONS_CHANNEL_ID)
-    if (user.id !== message.author.id && emoji.name === '💯') {
-      await notificationsChannel.send(`<@${user.id}> reacted to <@${message.author.id}>'s message with the ${emoji.name} emoji. <@${message.author.id}> earned +1 point`)
+    if (user.id !== message.author.id 
+      && user.id !== bot.user.id 
+      && emoji.name === '💯') {
+      await notificationsChannel.send(`<@${user.id}> reacted to <@${message.author.id}>'s message in the <#${message.channel.id}> channel (https://discord.com/channels/868130358640668713/${message.channel.id}/${message.id}) with the ${emoji.name} emoji. <@${message.author.id}> earned +1 point and now has a total of 0 points. `)
     }
   })
 }
