df6d57e7e0b936f3cf535bc955f6bc4525d0dfd3
Alex Booker <booker@booker.codes>
2021-09-29 05:41:51 +0100

    Refactor karma module

--- source/karma.js
+++ source/karma.js
@@ -11,25 +11,46 @@ const karma = bot => {
     const commands = guild.commands
     commands.create({
       name: 'karma',
-      description: 'Tells you how much karma you have'
+      description: 'Shows you how much karma you\'ve earned'
     })
+
     commands.create({
       name: 'leaderboard',
-      description: 'Tells you who has the most reputation'
+      description: 'Shows you the top 20 karma leaderboard'
     })
   })
 
   bot.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) { return }
-    const {commandName} = interaction
+    if (!interaction.isCommand()) { 
+      return 
+    }
+
+    const { commandName } = interaction
+
     if (commandName === 'karma') {
-      // interaction.user.id
       const rows = await knex('reputations')
         .where('to', interaction.user.id)
         .sum('points')
       const count = rows.shift().sum || 0
       interaction.reply({
         content: `You have ${count} reputation. Just ${250 - count} more to unlock a hoodie`,
+        ephemeral: true
+      })
+      return
+    }
+
+    if (commandName === 'leaderboard') {
+      const rows = await knex('reputations')
+        .select('to')
+        .sum({ totalPoints: 'points' })
+        .groupBy('to')
+        .orderBy('totalPoints', 'DESC')
+        .limit(20)
+      const leaderboardText = rows.reduce((prev, current, currentIndex) => {
+        return prev += `${currentIndex + 1}. <@${current.to}> has ${current.totalPoints} points\n`
+      }, '')
+      interaction.reply({
+        content: `.: Leaderboard :. \n\n${leaderboardText}`,
         ephemeral: true
       })
     }
@@ -39,18 +60,32 @@ const karma = bot => {
     if (messageReaction.partial) {
       await messageReaction.fetch()
     }
+
     const { message, emoji } = messageReaction
+
+    const reactionChannel = await bot
+      .channels
+      .fetch(message.channelId)
+    if (!reactionChannel.name.endsWith("-help")) {
+      return
+    }
+
     const notificationsChannel = await bot
       .channels
       .fetch(KARMA_NOTIFICATIONS_CHANNEL_ID)
+
     if (user.id !== message.author.id 
       && user.id !== bot.user.id 
       && emoji.name === '💜') {
 
-      const rows1 = await knex('reputations')
-        .where({ from: user.id, messageId: message.id })
-      if (rows1.length > 0) {
-      await notificationsChannel.send(`<@${user.id}> reacted to <@${message.author.id}>'s message but no more points were given lol`)
+      const karmaInteractions = await knex('reputations')
+        .where({ 
+          from: user.id,
+          messageId: message.id 
+        })
+      if (karmaInteractions.length > 0) {
+        // this user has already given karma for this
+        // message
         return
       }
 
@@ -61,18 +96,18 @@ const karma = bot => {
           to: message.author.id,
           messageId: message.id
         })
+
       const rows = await knex('reputations')
         .where('to', message.author.id)
         .sum('points')
-      const count = rows.shift().sum || 0 // should never be 0 since this code is only run in response to you getting some reputation in the first place lol
-      const exampleEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setAuthor('Scrimba', bot.user.displayAvatarURL(), 'https://discord.js.org')
-        .setDescription(`Well done <@${message.author.id}>! <@${user.id}> reacted to your post [post](https://example.com) in <#${message.channel.id}> with 💜 which earned you a point.
+      const count = rows.shift().sum || 0 //  should never be 0 since to get here the INSERT command above was run but feels better to have it 
+      const embed = new MessageEmbed()
+        .setColor('#C580F2')
+        .setAuthor('Pumpkin from Scrimba', bot.user.displayAvatarURL())
+        .setDescription(`Well done <@${message.author.id}>! <@${user.id}> reacted to your post [post](https://discord.com/channels/684009642984341525/${message.channel.id}/${message.id}) in <#${message.channel.id}> with 💜 which earned you a point.
 
-You now have ${count} karma!`)
-      await notificationsChannel.send({embeds: [exampleEmbed]})
-      // await notificationsChannel.send(`<@${user.id}> reacted to <@${message.author.id}>'s message in the <#${message.channel.id}> channel (https://discord.com/channels/684009642984341525/${message.channel.id}/${message.id}) with the ${emoji.name} emoji. <@${message.author.id}> earned +1 point and now has a total of ${count} points. `)
+You now have ${count} karma! To see your karma anytime type \`/karma\``)
+      await notificationsChannel.send({ embeds: [embed] })
     }
   })
 }
